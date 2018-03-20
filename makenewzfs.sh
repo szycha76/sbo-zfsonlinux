830d4981868448377813f0db6e10035be4f07914
Marcin Szychowski <szycha@gmail.com>
2018-03-21 00:41:38 +0100

    Lazy maintainer, v.0.0.1

--- makenewzfs.sh
+++ makenewzfs.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+function guessnewversion() {
+	txt=$(curl --silent http://zfsonlinux.org/ | grep -A4 '#80ff00'|tr /- "\n"|grep '[0-9]\+.*<$'|sed 's/<$//'|uniq)
+	c=$(echo "$txt"|wc -l)
+	if [ '1' == "$c" ]; then
+		echo $txt
+	fi
+}
+
 set -e
 pwd=$(pwd)
 wd=$(mktemp -d)
 cd $wd
 
-NEWVERSION=${NEWVERSION:-0.7.7}
+guessed=$(guessnewversion)
+NEWVERSION=${NEWVERSION:-${guessed:-0.7.7}}
 
 KERN=${KERN:-"$(uname -r)"}
 PKGTYPE=${PKGTYPE:-txz}
@@ -23,6 +32,11 @@ for sbo in spl-solaris zfs-on-linux; do
 		cd $sbo
 		VERSION=$(grep ^VERSION= $sbo.info|cut -d'"' -f2|sed 's:\.:\\.:g')
 		sed -i.old s/$VERSION/$NEWVERSION/g $sbo.info $sbo.SlackBuild
+		sums=$(md5sum $sbo.info $sbo.info.old|cut -d ' ' -f1|uniq|wc -l)
+		if [ "1" == "$sums" ]; then
+			echo Nothing has changed yet, leave it.
+			exit 1
+		fi
 		srcurl=$(grep DOWNLOAD= $sbo.info|cut -d'"' -f2)
 		wget --no-check-certificate "$srcurl"
 		MD5SUM=$(md5sum *.gz |cut -d' ' -f1)
